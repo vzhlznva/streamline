1d28b63f5a781774f008cdf78035e9742ea7a348
Valeriia Zhelieznova <lzheleznova2609@gmail.com>
2025-07-17 14:20:35 +0300

    build: vite and ts configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
   plugins: [
     vue({
       include: [/\.vue$/],
